64023b25ca1c1c25b46f9fb9d9e89542bc0b5973
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2018-03-14 08:51:29 +0100

    upgrade to Spring Boot 1.5, new style app launcher and pom tweaks

--- src/main/java/deors/services/heartbeat/HeartbeatController.java
+++ src/main/java/deors/services/heartbeat/HeartbeatController.java
@@ -4,12 +4,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HeartbeatController {
 
     private final AtomicLong counter = new AtomicLong();
